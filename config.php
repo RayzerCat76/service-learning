9dbfba328e89834a7090523a20e5f0120bd508b5
RayzerCat76 <ray@rayliu.top>
2026-03-24 15:11:50 +0800

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "service_learning";
-
-$conn = new mysqli($host, $user, $pass, $dbname);
-if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
-?>
