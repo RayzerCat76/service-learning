f9c6cfb969b259adbe71cf51d4379dc2135624b1
RayzerCat76 <ray@rayliu.top>
2026-03-24 15:26:37 +0800

    Refactor login function to use new SQL import

--- api/login.js
+++ api/login.js
@@ -1,15 +1,17 @@
-import { getDB } from './db';
+import { sql } from './db.js';
 import jwt from 'jsonwebtoken';
 
-export default async function(req,res) {
+export default async (req, res) => {
   const { username, password } = await req.json();
-  const sql = getDB();
-  const user = await sql`
+  const users = await sql`
     SELECT * FROM users WHERE username = ${username} AND password = ${password}
   `;
-  if (user.length === 0) return res.json({ success:false });
-  const token = jwt.sign({ id:user[0].id }, process.env.JWT_SECRET);
+  if (users.length === 0) return res.json({ success: false });
+  const token = jwt.sign({ id: users[0].id }, process.env.JWT_SECRET);
   res.json({
-    token, name:user[0].name, role:user[0].role
+    success: true,
+    token,
+    name: users[0].name,
+    role: users[0].role
   });
-}
+};
