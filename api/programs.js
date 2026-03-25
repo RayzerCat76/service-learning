a1dacc7b43fe77feaf37c417206f14a595fbe94e
RayzerCat76 <ray@rayliu.top>
2026-03-25 08:49:59 +0800

    Update programs.js

--- api/programs.js
+++ api/programs.js
@@ -1,5 +1,6 @@
 import { sql } from './db.js';
-export default async (req,res)=>{
-  res.setHeader('Access-Control-Allow-Origin','*');
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.json(await sql`SELECT * FROM programs`);
-};
+}
