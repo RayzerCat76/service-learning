ebf17765e039afa77609b455d5ad6f56c5f56094
RayzerCat76 <ray@rayliu.top>
2026-03-25 08:51:24 +0800

    Update blocks.js

--- api/blocks.js
+++ api/blocks.js
@@ -1,6 +1,7 @@
 import { sql } from './db.js';
-export default async (req,res)=>{
-  res.setHeader('Access-Control-Allow-Origin','*');
-  const {program_id}=req.query;
-  res.json(await sql`SELECT * FROM blocks WHERE program_id=${program_id}`);
-};
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const program_id = req.query.program_id;
+  res.json(await sql`SELECT * FROM blocks WHERE program_id = ${program_id}`);
+}
