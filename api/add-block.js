d565e64368924004f0ad4888a336bede17e34ee7
RayzerCat76 <ray@rayliu.top>
2026-03-25 08:36:58 +0800

    Update add-block.js

--- api/add-block.js
+++ api/add-block.js
@@ -1,10 +1,7 @@
 import { sql } from './db.js';
-
-export default async (req, res) => {
-  const { program_id } = await req.json();
-  await sql`
-    INSERT INTO blocks (program_id, title, content, bg, text, border, x, y, w, h)
-    VALUES (${program_id}, 'New Block', '', '#fff', '#000', '#ddd', 20, 20, 60, 25)
-  `;
-  res.json({ success: true });
+export default async (req,res)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  const {program_id}=await req.json();
+  await sql`INSERT INTO blocks (program_id,title,content,bg,text,border,x,y,w,h) VALUES (${program_id},'New Block','','#fff','#000','#ddd',20,20,60,25)`;
+  res.json({success:true});
 };
