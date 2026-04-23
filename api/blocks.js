97f58ea6c3be889aabcc8178fc1ca79a03af4c91
RayzerCat76 <ray@rayliu.top>
2026-04-23 10:49:53 +0800

    Delete api directory

--- api/blocks.js
+++
@@ -1,7 +0,0 @@
-import { sql } from './db.js';
-
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  const program_id = req.query.program_id;
-  res.json(await sql`SELECT * FROM blocks WHERE program_id = ${program_id}`);
-}
